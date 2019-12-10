fc39eb5a19ea6e065de4a17f051a93e5b32558fd
mikezks <michael.zikes@intauria.com>
2019-12-10 11:08:18 +0100

    Angular Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'flight-app',
+  selector: 'app-flight',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  title = 'Hello World!';
+  title = 'Hello Angular!';
+  content = 'Das ist mein neuer Textblock!';
+
+  changeContent(value: string): void {
+    this.content = value;
+  }
 }
